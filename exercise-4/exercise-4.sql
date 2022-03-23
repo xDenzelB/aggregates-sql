0a54637b9eb3eaa636b18d6aa9a454768c31176b
Denzel <dbartolaba123@gmail.com>
2022-03-23 12:54:10 -0700

    got 4 tests to pass

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,18 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+category.name, 
+AVG(film.length)
+FROM 
+category
+LEFT JOIN 
+film_category 
+ON 
+film_category.category_id = category.category_id
+LEFT JOIN 
+film
+ON 
+film.film_id = film_category.film_id
+GROUP BY
+category.name
+
