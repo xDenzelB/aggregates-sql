0e14bf0fed38b567afc1b3d26674d42ccc07f8f3
Denzel <dbartolaba123@gmail.com>
2022-03-23 14:19:46 -0700

    got my 5th test to pass

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,13 @@
 -- the total run time of all inventory items if they were all played back to back
+SELECT
+SUM(film.length)
+FROM 
+inventory
+LEFT JOIN 
+film
+ON 
+film.film_id = inventory.film_id
+ORDER BY
+SUM DESC 
+LIMIT
+10;
