7226474172baae62e8ccbb36c0f56d8a2cbdc7b9
Denzel <dbartolaba123@gmail.com>
2022-03-23 15:19:07 -0700

    one more test to go

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,21 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+SELECT 
+email,
+RANK() OVER (ORDER BY SUM(length) DESC)
+FROM
+customer 
+LEFT JOIN 
+rental
+ON 
+rental.customer_id = customer.customer_id
+LEFT JOIN 
+inventory
+ON 
+inventory.inventory_id = rental.inventory_id
+LEFT JOIN 
+film 
+ON 
+film.film_id = inventory.film_id
+GROUP BY 
+customer.email
--- exercise-9/exercise-9.sql
+++ exercise-9/exercise-9.sql
@@ -1,2 +1,16 @@
 -- ROLLUP of amount made per year, month, day
 -- use ROLLUP and EXTRACT YEAR, MONTH, DAY from payment_date
+SELECT
+EXTRACT (YEAR FROM payment_date) y,  
+EXTRACT (MONTH FROM payment_date) m,  
+EXTRACT (DAY FROM payment_date) d,
+SUM(amount)
+FROM 
+payment 
+GROUP BY 
+ROLLUP(
+    EXTRACT (YEAR FROM payment_date),  
+EXTRACT (MONTH FROM payment_date),  
+EXTRACT (DAY FROM payment_date)
+)
+
